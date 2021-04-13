2387250cc07849d55d102350a21708f3293a96c8
Önder Eser <64664269+ondereser@users.noreply.github.com>
2021-04-14 01:24:57 +0300

    Add files via upload

--- TrainingSimulation.py
+++ TrainingSimulation.py
@@ -1,4 +1,5 @@
 from utilities import *
+from sklearn.model_selection import train_test_split
 
 # Step 1
 path = "project/myData/"
@@ -6,3 +7,14 @@ data = importDataInfo(path)
 
 # Step 2
 data = balanceData(data, display = False)
+
+# Step 3
+imagesPath, steerings = loadData(path, data)
+#print(imagesPath[0], steering[0])
+
+# Step 4
+xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steerings, test_size=0.2, random_state=5)
+#print("Total Training Images:", len(xTrain))
+#print("Total Validation Images:", len(xVal))
+
+# Step 5
--- utilities.py
+++ utilities.py
@@ -3,6 +3,9 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt  
 from sklearn.utils import shuffle
+import matplotlib.image as mpimg
+from imgaug import augmenters as iaa
+import cv2
 
 def getName(filePath):
     return filePath.split("\\")[-1]
@@ -15,7 +18,7 @@ def importDataInfo(path):
     #print(getName(data["Center"][0]))
     data["Center"] = data["Center"].apply(getName)
     #print(data.head())
-    print("total images imported:", data.shape[0])
+    #print("total images imported:", data.shape[0])
     return data
 
 def balanceData(data, display = True):
@@ -39,9 +42,9 @@ def balanceData(data, display = True):
         binDataList = shuffle(binDataList)
         binDataList = binDataList[500:]
         removeIndexList.extend(binDataList)
-    print("removed images:", len(removeIndexList))
+    #print("removed images:", len(removeIndexList))
     data.drop(data.index[removeIndexList], inplace = True)
-    print("remaining images:", len(data))
+    #print("remaining images:", len(data))
    
     if display: 
         hist, _ = np.histogram(data["Steering"], nBins)
@@ -49,3 +52,43 @@ def balanceData(data, display = True):
         plt.plot((-1,1),(samplesPerBin, samplesPerBin))
         plt.show()
     return data
+
+def loadData(path, data):
+    imagesPath = []
+    steering = []
+
+    for i in range (len(data)):
+        indexedData = data.iloc[i]
+        #print(indexedData)
+        imagesPath.append(os.path.join(path, "IMG", indexedData[0]))
+        #print(os.path.join(path, "IMG", indexedData[0]))
+        steering.append(float(indexedData[3]))
+    imagesPath = np.asarray(imagesPath)
+    steering = np.asarray(steering)
+    return imagesPath, steering
+
+def augmentImage(imgPath, steering):
+    img = mpimg.imread(imgPath)
+    #print(np.random.rand(),np.random.rand(),np.random.rand(),np.random.rand())
+    ## PAN
+    if np.random.rand() < 0.5:
+        pan = iaa.Affine(translate_percent={"x": (-0.1, 0.1), "y": (-0.1, 0.1)})
+        img = pan.augment_image(img)
+    ## ZOOM
+    if np.random.rand() < 0.5:
+        zoom = iaa.Affine(scale=(1,1.2))
+        img = zoom.augment_image(img)
+    ## BRIGHTNESS
+    if np.random.rand() < 0.5:
+        brightness = iaa.Multiply((0.4,1.2))
+        img = brightness.augment_image(img)
+    ## FLIP 
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
+        steering = -steering
+    
+    return img, steering
+   
+# imgRe, st = augmentImage("C:\\Users\\Onder\\Desktop\\self-driving-car-sim\\env\\test.jpg", 0)
+# plt.imshow(imgRe)
+# plt.show()
