3e5c9aa65dc12400a87c0a6b65a5ed9bda1c3210
Önder Eser <64664269+ondereser@users.noreply.github.com>
2021-04-14 00:33:38 +0300

    Add files via upload

--- TrainingSimulation.py
+++ TrainingSimulation.py
@@ -3,3 +3,6 @@ from utilities import *
 # Step 1
 path = "project/myData/"
 data = importDataInfo(path)
+
+# Step 2
+data = balanceData(data, display = False)
--- utilities.py
+++ utilities.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 import numpy as np 
 import os
+import matplotlib.pyplot as plt  
+from sklearn.utils import shuffle
 
 def getName(filePath):
     return filePath.split("\\")[-1]
@@ -14,4 +16,36 @@ def importDataInfo(path):
     data["Center"] = data["Center"].apply(getName)
     #print(data.head())
     print("total images imported:", data.shape[0])
+    return data
+
+def balanceData(data, display = True):
+    nBins = 31  # tek sayı olmalı
+    samplesPerBin = 500
+    hist, bins = np.histogram(data["Steering"], nBins)
+    #print(bins)
+    center = (bins[:-1] + bins[1:])*0.5
+    if display: 
+        #print(center)
+        plt.bar(center, hist, width = 0.06)
+        plt.plot((-1,1),(samplesPerBin, samplesPerBin))
+        plt.show()
+    
+    removeIndexList = []
+    for j in range(nBins):
+        binDataList = []
+        for i in range(len(data["Steering"])):
+            if data["Steering"][i] >= bins[j] and data["Steering"][i] <= bins[j+1]:
+                binDataList.append(i)
+        binDataList = shuffle(binDataList)
+        binDataList = binDataList[500:]
+        removeIndexList.extend(binDataList)
+    print("removed images:", len(removeIndexList))
+    data.drop(data.index[removeIndexList], inplace = True)
+    print("remaining images:", len(data))
+   
+    if display: 
+        hist, _ = np.histogram(data["Steering"], nBins)
+        plt.bar(center, hist, width = 0.06)
+        plt.plot((-1,1),(samplesPerBin, samplesPerBin))
+        plt.show()
     return data
